7e1a06f8c219d0699f6dd61e248bc8bf8ad17ea3
marlon avila <marlonavila012@gmail.com>
2023-08-12 21:55:51 -0300

    Initial commit

---
+++ desafio-lanchonete-2023/src/item.js
@@ -0,0 +1,18 @@
+export default class item{ // coleta todas ás informações dos itens
+
+    codigo;
+    descricao;
+    valor;
+    quantidade;
+
+    constructor(codigo, quantidade) {
+        this.codigo=codigo;
+        this.quantidade=quantidade;
+    }
+    constructor(codigo, descricao, valor, quantidade) {
+        this.codigo=codigo;
+        this.quantidade=quantidade;
+        this.descricao=descricao;
+        this.valor=valor;
+    }
+}
